2fe0b4155c77d5f2888757efdc11c16c4a8bc3fd
Alexandra Fren <alexandra.fren@gmail.com>
2019-03-27 18:51:53 +0000

    Automatically backed up by Learn

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -12,26 +12,26 @@ function booksReducer(state = [], action) {
   switch (action.type) {
     case "ADD_BOOK":
       return [...state, action.book];
- 
+
     case "REMOVE_BOOK":
       idx = state.indexOf(action.id);
       return [...state.slice(0, idx), ...state.slice(idx + 1)];
- 
+
     default:
       return state;
   }
 }
- 
+
 function authorsReducer(state = [], action) {
   let idx;
   switch (action.type) {
     case "ADD_AUTHOR":
       return [...state, action.author];
- 
+
     case "REMOVE_AUTHOR":
       idx = state.indexOf(action.id);
       return [...state.slice(0, idx), ...state.slice(idx + 1)];
- 
+
     default:
       return state;
   }
